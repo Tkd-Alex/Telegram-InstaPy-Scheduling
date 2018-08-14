ed59f4ddb42443447f3ed34a60a0ddde72ae2441
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-08-14 12:47:11 +0200

    Perform thread for instapy

--- thread.py
+++ thread.py
@@ -1,24 +1,22 @@
-import threading, datetime, json
+import threading, datetime, json, time
+from scripts import scripts
 
 class Thread (threading.Thread):
-    def __init__(self, name):
+    def __init__(self, job_name, script_name, username, password, bot, chat_id):
         threading.Thread.__init__(self)
-        self.name = name
-    
-    def set_elegram(self, bot, chat_id):
+        self.job_name = job_name
+        self.script_name = script_name
+        self.username = username
+        self.password = password
         self.bot = bot
-        self.chat_id = chat_id
+        self.chat_id = chat_id 
     
     def run(self):
         start = datetime.datetime.now().replace(microsecond=0)
         self.bot.send_message(self.chat_id, text='InstaPy Bot - {} start at {}'.format(self.name, time.strftime("%X")))
         
-        if self.name.lower() == "follow-trick":
-        	instapy_script.follow_trick(config.get('instapy', 'username'), config.get('instapy', 'password'))
-        else:
-        	instapy_script.daily(config.get('instapy', 'username'), config.get('instapy', 'password'))
-
-        endTime = time.strftime("%X")
+        scripts[self.script_name](self.username, self.password)
+        
         end = datetime.datetime.now().replace(microsecond=0)
         self.bot.send_message(self.chat_id, text='InstaPy Bot end at {}\nExecution time {}'.format(time.strftime("%X"), end-start))
         
