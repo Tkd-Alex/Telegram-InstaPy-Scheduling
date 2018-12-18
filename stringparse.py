af6e56a77f2d122a5e525d9457db8ee33ddf9a04
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-12-18 22:08:53 +0100

    Clear logs if the script not eding with SESSION ENDED

--- stringparse.py
+++ stringparse.py
@@ -24,14 +24,25 @@ On session start was FOLLOWING 6708 users & had 17371 FOLLOWERS
 OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 INFO [2018-12-05 19:22:48] [tkd_alex]  Session ended!
 oooooooooooooooooooooooooooooooooooooooooooooooooooo
+_____________________________________________________________
+
+INFO [2018-12-16 18:28:29] [tkd_alex]  --> Total people unfollowed : 397
+
 """
-def parse_loglines(lines):
+def parse_loglines(lines, username=None):
     # Merge the string by new line char. Keep the text between 'sessional live report' and 'OOOOOO'
     message = '\n'.join(x for x in lines)
-    message = message[message.find('Sessional Live Report'):message.find('OOOOOO')]
-    lines = message.split('\n')
-    message = '\n'.join(x.strip() for x in lines if x.strip() != "")
-    for boldword in [ "LIKED", "COMMENTED", "FOLLOWED", "UNFOLLOWED", "INAPPROPRIATE", "NOT VALID" ]:
-        message = message.replace("ALREADY {}".format(boldword), "<b>ALREADY {}</b>".format(boldword))
-        message = message.replace("|> {}".format(boldword), "|> <b>{}</b>".format(boldword))
-    return message
+    if 'Session ended!' in message:
+        message = message[message.find('Sessional Live Report'):message.find('OOOOOO')]
+        lines = message.split('\n')
+        message = '\n'.join(x.strip() for x in lines if x.strip() != "")
+        for boldword in [ "LIKED", "COMMENTED", "FOLLOWED", "UNFOLLOWED", "INAPPROPRIATE", "NOT VALID" ]:
+            message = message.replace("ALREADY {}".format(boldword), "<b>ALREADY {}</b>".format(boldword))
+            message = message.replace("|> {}".format(boldword), "|> <b>{}</b>".format(boldword))
+        return message
+    else:
+        if username != None:
+            message = message[message.find("[{}]".format(username)):] # Remove INFO [2018-12-16 18:28:29] [tkd_alex]
+        lines = message.replace('-->', '').split('\n') # Remove arrow
+        message = '\n'.join(x.strip() for x in lines if x.strip() != "") # Clear string line by line
+        return message.split('\n')[-1] # Return the last line of logs.
