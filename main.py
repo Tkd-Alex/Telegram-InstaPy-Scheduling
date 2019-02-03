0086a0c26ca003bf75d0dbd6a2be6ce5f7f2d214
Alessandro Maggio <alex.tkd.alex@gmail.com>
2019-02-03 21:48:03 +0100

    Persistent save jobs. Reload with single command

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import random, sys, os, pickle, plac
 # Colored terminal
 from termcolor import colored, cprint
 
+# Database Jobs and Users storage
+from tinydb import TinyDB, Query
+
 # Telegram imports
 from telegram.ext import Updater, CommandHandler, Job, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
@@ -16,7 +19,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from process import Process, reload_process
 
 from stringparse import parse_time, clear_lines
-from utils import safe_load_settings
+import utils
 
 # Create array of all users
 users = []
@@ -32,6 +35,8 @@ dict_settings = {
     "users_path": "users.pickle",
     "project_path": "./"
 }
+
+database = None
 
 def help(bot, update):
     update.message.reply_text('Hi! Use /set to start the bot')
@@ -125,7 +130,7 @@ def exec_process(bot, job):
 
 def create_process(bot, context):
     process_array[context['job_name']] = Process(
-        instapy_folder,
+        dict_settings['instapy_folder'],
         context['job_name'],
         context['script_name'],
         context['chat_id'],
@@ -161,7 +166,7 @@ def status_process(bot, update, args):
         message = 'You have not the permission to use this bot.\nFor more details visit [Telegram-InstaPy-Scheduling](https://github.com/Tkd-Alex/Telegram-InstaPy-Scheduling)'
         update.message.reply_text(message, parse_mode='Markdown')  
 
-def set(bot, update, args, job_queue, chat_data):
+def set_job(bot, update, args, job_queue, chat_data):
     if str(update.message.chat_id) in dict_settings['allowed_id']:
         try:
             usernames = [ a['username'].lower() for a in users ]
@@ -169,10 +174,11 @@ def set(bot, update, args, job_queue, chat_data):
                 update.message.reply_text("Sorry, username <b>{}</b> is not saved.".format(args[0]), parse_mode='HTML')
                 return
 
-            if args[1] in chat_data or args[1] in process_array:
+            jobs_list = utils.return_elements(database, owner=str(update.message.chat_id), entity="job")
+            if args[1] in chat_data or args[1] in jobs_list or args[1] in process_array:
                 update.message.reply_text("Sorry, job named <b>{}</b> is already used.".format(args[1]), parse_mode='HTML')
                 return
-
+            
             if not args[2] in scripts:
                 update.message.reply_text("Sorry, script named <b>{}</b> is not in your scripts file.".format(args[2]), parse_mode='HTML')
                 return
@@ -203,47 +209,59 @@ def set(bot, update, args, job_queue, chat_data):
         message = 'You have not the permission to use this bot.\nFor more details visit [Telegram-InstaPy-Scheduling](https://github.com/Tkd-Alex/Telegram-InstaPy-Scheduling)'
         update.message.reply_text(message, parse_mode='Markdown')
 
-def day_choose(bot, update, job_queue, chat_data):
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    
-    query = update.callback_query
-    
-    scheduled_time = parse_time(chat_data['tmpjob']['scheduled'])
-    name_job = chat_data['tmpjob']['job_name']
-
-    if query.data == '-1' or query.data == '-2':
+def persistend_job(bot, job_queue, chat_data, days, chat_id, message_id=None, db_persist=True):
+    if chat_data['tmpjob']['job_name'] not in chat_data:
         context = {
             "job_name": chat_data['tmpjob']['job_name'],
             "script_name": chat_data['tmpjob']['script_name'],
             "user": None,
-            "chat_id": query.message.chat_id,
+            "chat_id": chat_id,
         }
-
+        
         for user in users:
             if user['username'].lower() == chat_data['tmpjob']['username']:
                 context['user'] = user
                 break
 
         create_process(bot, context)
-    
-        if query.data == '-1':
-            job = job_queue.run_daily(exec_process, scheduled_time, context=context, name=name_job)
+        job_name = chat_data['tmpjob']['job_name']
+        scheduled_time = parse_time(chat_data['tmpjob']['scheduled'])
+
+        # DAYS MANAGEMENT REQUIRE A REFACTORY.
+
+        if chat_data['tmpjob']['days'] == "Everyday":
+            job = job_queue.run_daily(exec_process, scheduled_time, context=context, name=job_name)
+            selected_days = "Everyday"
         else:
-            selected_days = ", ".join([days[i] for i in chat_data['tmpjob']['days']])
-            job = job_queue.run_daily(exec_process, scheduled_time, days=tuple(chat_data['tmpjob']['days']), context=context, name=name_job)
+            chat_data['tmpjob']['days'] = utils.fix_days(days)
+            selected_days = ", ".join([utils.days[i] for i in chat_data['tmpjob']['days']])
+            job = job_queue.run_daily(exec_process, scheduled_time, days=tuple(chat_data['tmpjob']['days']), context=context, name=job_name)
 
         data = {
-            'name': name_job,
+            'name': job_name,
             "script_name": chat_data['tmpjob']['script_name'],
             'scheduled': chat_data['tmpjob']['scheduled'],
             "username": chat_data['tmpjob']['username'],
-            'job': job, 
-            'days': "Everyday" if query.data == '-1' else selected_days 
         }
-        chat_data[name_job] = data
+
+        data['days'] = selected_days
+        
+        if db_persist is True:
+            utils.save_element(database, data, owner=chat_id, entity="job")
+            bot.edit_message_text(text="Job <b>{}</b> setted!".format(job_name), chat_id=chat_id, message_id=message_id, parse_mode='HTML')
+        else:
+            bot.send_message(text="Job <b>{}</b> setted!".format(job_name), chat_id=chat_id, parse_mode='HTML')
+        
+        data['job'] = job
+        chat_data[job_name] = data
         del chat_data['tmpjob']
 
-        bot.edit_message_text(text="Job <b>{}</b> setted!".format(name_job), chat_id=query.message.chat_id, message_id=query.message.message_id, parse_mode='HTML')
+def day_choose(bot, update, job_queue, chat_data):
+    query = update.callback_query
+    
+    if query.data == '-1' or query.data == '-2':
+        chat_data['tmpjob']['days'] = "Everyday" if query.data == '-1' else None
+        persistend_job(bot, job_queue, chat_data, days=chat_data['tmpjob']['days'], chat_id=query.message.chat_id, message_id=query.message.message_id)
     else:
         if int(query.data) not in chat_data['tmpjob']['days']:
             chat_data['tmpjob']['days'].append(int(query.data))
@@ -262,23 +280,36 @@ def day_choose(bot, update, job_queue, chat_data):
                             chat_id = query.message.chat_id,
                             message_id = query.message.message_id,
                             reply_markup = InlineKeyboardMarkup(keyboard))
-
+    
 def unset(bot, update, args, chat_data):
     if str(update.message.chat_id) in dict_settings['allowed_id']:
         try:
-            name_job = args[0]
-            if name_job in chat_data and name_job in process_array:
-                job = chat_data[name_job]["job"]
+            job_name = args[0]
+            if job_name in chat_data and job_name in process_array:
+                job = chat_data[job_name]["job"]
                 job.schedule_removal()
 
-                del process_array[name_job]
-                del chat_data[name_job]
+                del process_array[job_name]
+                del chat_data[job_name]
 
-                update.message.reply_text('Job <b>{}</b> successfully unset!'.format(name_job), parse_mode='HTML')
+                utils.delete_job(database, name_jon, owner=str(update.message.chat_id))
+
+                update.message.reply_text('Job <b>{}</b> successfully unset!'.format(job_name), parse_mode='HTML')
             else:
-                update.message.reply_text("Sorry, job named <b>{}</b> was not found.".format(name_job), parse_mode='HTML')
+                update.message.reply_text("Sorry, job named <b>{}</b> was not found.".format(job_name), parse_mode='HTML')
         except (IndexError, ValueError):
-            update.message.reply_text('Usage: /unset <name_job>')   
+            update.message.reply_text('Usage: /unset <job_name>')   
+    else:
+        message = 'You have not the permission to use this bot.\nFor more details visit [Telegram-InstaPy-Scheduling](https://github.com/Tkd-Alex/Telegram-InstaPy-Scheduling)'
+        update.message.reply_text(message, parse_mode='Markdown')
+
+def reload_jobs(bot, update, job_queue, chat_data):
+    if str(update.message.chat_id) in dict_settings['allowed_id']:
+        jobs_list = utils.return_elements(database, owner=str(update.message.chat_id), entity="job")
+        for singlejob in jobs_list:
+            chat_data['tmpjob'] = singlejob
+            chat_data['tmpjob']['job_name'] = chat_data['tmpjob']['name'] 
+            persistend_job(bot, job_queue, chat_data, days=chat_data['tmpjob']['days'], chat_id=update.message.chat_id, db_persist=False)
     else:
         message = 'You have not the permission to use this bot.\nFor more details visit [Telegram-InstaPy-Scheduling](https://github.com/Tkd-Alex/Telegram-InstaPy-Scheduling)'
         update.message.reply_text(message, parse_mode='Markdown')
@@ -361,6 +392,7 @@ def main(setting_file='settings.json'):
     global scripts
     global users
     global dict_settings
+    global database
 
     try:
         cprint("Load setting file from: \"%s\"" % setting_file, "green" )
@@ -371,7 +403,7 @@ def main(setting_file='settings.json'):
         sys.exit(1)
 
     for key in dict_settings:
-        result, value, message = safe_load_settings(key, settings_json)
+        result, value, message = utils.safe_load_settings(key, settings_json)
         if result is False:
             cprint("[ERROR] {}".format(message), "red")
             if key not in ["telegram_token", "instapy_folder", "allowed_id"]:
@@ -400,6 +432,8 @@ def main(setting_file='settings.json'):
         pickle.dump(users, open(dict_settings['project_path'] + dict_settings['users_path'], 'wb'))
         cprint("[WARNING] Init user list in: {}".format(dict_settings['users_path']), "yellow" )
 
+    database = TinyDB('./db.json')
+
     updater = Updater(dict_settings['telegram_token'], request_kwargs={'read_timeout': 20, 'connect_timeout': 20})
 
     dp = updater.dispatcher
@@ -410,12 +444,13 @@ def main(setting_file='settings.json'):
     dp.add_handler(CommandHandler("status", status_process, pass_args=True))
     dp.add_handler(CommandHandler("logs", logs,  pass_args=True))
 
-    dp.add_handler(CommandHandler("set", set, pass_args=True, pass_job_queue=True, pass_chat_data=True))
+    dp.add_handler(CommandHandler("set", set_job, pass_args=True, pass_job_queue=True, pass_chat_data=True))
     dp.add_handler(CommandHandler("now", now, pass_args=True))
     dp.add_handler(CommandHandler("stop", stop, pass_args=True))
 
     dp.add_handler(CommandHandler("unset", unset, pass_args=True, pass_chat_data=True))
     dp.add_handler(CommandHandler("jobs", list_jobs, pass_chat_data=True))
+    dp.add_handler(CommandHandler("reload", reload_jobs, pass_job_queue=True, pass_chat_data=True))
 
     dp.add_handler(CommandHandler("add_user", add_user, pass_args=True))
     dp.add_handler(CommandHandler("delete_user", delete_user, pass_args=True))
@@ -437,4 +472,3 @@ def main(setting_file='settings.json'):
         
 if __name__ == '__main__':
     plac.call(main)
-
--- utils.py
+++ utils.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from tinydb import Query, where
+
 def safe_load_settings(field, settings):
 	if field in settings:
 		return True, settings[field], "Load {} : {}".format(field, settings[field])
 	return False, None, "Unable to load {}".format(field) 
+
+def return_elements(database, owner, entity="job"):
+	query = Query()
+	items = database.search((where('entity') == entity) & (where('owner') == owner)) 
+	return items
+
+def save_element(database, data, owner, entity="job"):
+	data['entity'] = entity
+	data['owner'] = str(owner)
+	database.insert( data )
+
+def delete_job(database, name, owner):
+	database.remove((where('entity') == "job") & (where('owner') == owner) & (where('name') == name))
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+def fix_days(days_args):
+	if type(days_args[0]) == int:
+		return days_args
+	return [ days.index(day.strip()) for day in days_args.split(',') ]
